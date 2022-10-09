83c02fe71e9f322fdf76dff2354505a21928a066
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2022-10-08 20:11:49 -0700

    added add/close dropdown, build

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,6 +26,14 @@ function App() {
     }
   ])
 
+  //Add Task
+  const addTask = task => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    // console.log(id)
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //Toggle Reminder
   const toggleReminder = id => {
     setTasks(tasks.map(task => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
@@ -37,8 +46,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "You have no Tasks Added!"}
     </div>
   )
